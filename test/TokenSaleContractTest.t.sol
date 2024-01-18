85bab9e510e3ffb2f8765ef110ae37cd57167d7c
anjanayraina <anjanayraina326@gmail.com>
2024-01-18 08:15:59 +0530

    but and distribute functions added

--- test/TokenSaleContractTest.t.sol
+++ test/TokenSaleContractTest.t.sol
@@ -13,6 +13,8 @@ contract TokenSaleContractTest is Test {
     function setUp() public {
         token = new SupraOracleToken(address(this));
         tokenSale = new TokenSaleContract(IERC20(token), address(this));
+        vm.startPrank(address(this));
+        token.mint(address(tokenSale) , 1000*(10**token.decimals()));
     }
 
     function testInitialState() public {
@@ -39,4 +41,32 @@ contract TokenSaleContractTest is Test {
         assertFalse(tokenSale.isPreSaleActive());
         assertFalse(tokenSale.isPublicSaleActive());
     }
+
+    function testBuyTokens() public {
+    vm.startPrank(address(this));
+      tokenSale.changePreSaleStatus(true);
+    vm.stopPrank();
+    vm.deal(address(0x123) , 20 ether);
+        vm.startPrank(address(0x123));
+      tokenSale.buyTokens{value: 10 ether}();
+      assertTrue(tokenSale.isPreSaleActive());
+      assertEq(tokenSale.contributions(address(0x123)), 10 ether);
+      assertEq(token.balanceOf(address(0x123)) , 100 ether);
+      vm.stopPrank();
+  }
+
+  function testDistributeTokens() public {
+      vm.startPrank(address(this));
+      tokenSale.distributeTokens(address(this), 100*(10**token.decimals()));
+      assertEq(token.balanceOf(address(this)), 100*(10**token.decimals()));
+      vm.stopPrank();
+  }
+
+//   function testRefund() public {
+//       tokenSale.changePreSaleStatus(true);
+//       tokenSale.buyTokens{value: 10 ether}(addr1);
+//       tokenSale.changePublicSaleStatus(false);
+//       tokenSale.refund(10 ether);
+//       assertEqual(tokenSale.contributions(addr1), 0);
+//   }
 }
